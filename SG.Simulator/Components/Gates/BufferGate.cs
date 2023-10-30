eaa53f5229f732fa6386a5cbf7ea86ca54f859a8
Kelmar Firesun <kfiresun@gmail.com>
2023-10-30 11:58:25 -0500

    Adding Buffer and NOT gates.

---
+++ SG.Simulator/Components/Gates/BufferGate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SG.Simulator.Components.Gates
+{
+    public class BufferGate : Component
+    {
+        private readonly Pin m_input;
+
+        public BufferGate(Circuit circuit)
+            : base(circuit)
+        {
+            m_input = new Pin(this, PinType.Input);
+
+            Output = new Pin(this, PinType.Output);
+
+            Pins.Add(m_input);
+            Pins.Add(Output);
+        }
+
+        public Pin Output { get; }
+
+        public override void Update()
+        {
+            LineState state = m_input.FiniteState;
+
+            if (state.IsError())
+                Output.SetState(LineState.Floating);
+            else
+            {
+                // Mirror the state on our output.
+                Output.SetState(state);
+            }
+        }
+    }
+}
---
+++ SG.Simulator/Components/Gates/NotGate.cs
@@ -0,0 +1,33 @@
+﻿namespace SG.Simulator.Components.Gates
+{
+    public class NotGate : Component
+    {
+        private readonly Pin m_input;
+
+        public NotGate(Circuit circuit)
+            : base(circuit)
+        {
+            m_input = new Pin(this, PinType.Input);
+
+            Output = new Pin(this, PinType.Output);
+
+            Pins.Add(m_input);
+            Pins.Add(Output);
+        }
+
+        public Pin Output { get; }
+
+        public override void Update()
+        {
+            LineState state = m_input.FiniteState;
+
+            if (state.IsError() || state == LineState.Floating)
+                Output.SetState(LineState.Floating);
+            else
+            {
+                // Invert the state on our output.
+                Output.SetState(state == LineState.Low ? LineState.High : LineState.Low);
+            }
+        }
+    }
+}
