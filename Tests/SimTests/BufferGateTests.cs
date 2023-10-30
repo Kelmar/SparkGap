91c8df3cc2a90e7bb683fd8cd44a3cc6d796302c
Kelmar Firesun <kfiresun@gmail.com>
2023-10-30 15:26:41 -0500

    Tests for NOT and Buffer gates

---
+++ Tests/SimTests/BufferGateTests.cs
@@ -0,0 +1,71 @@
+﻿using NUnit.Framework;
+
+using SG.Simulator.Components.Gates;
+using SG.Simulator.Components;
+using SG.Simulator;
+using System.Linq;
+
+namespace SimTests
+{
+    internal class BufferGateTests
+    {
+        private Circuit m_circuit;
+        private BufferGate m_bufGate;
+
+        private Wire m_wire;
+
+        [SetUp]
+        public void Setup()
+        {
+            m_circuit = new Circuit();
+            m_bufGate = new BufferGate(m_circuit);
+            m_wire = new Wire(m_circuit);
+
+            m_wire.Pins.Add(m_bufGate.InputPins.Single());
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            m_circuit.Dispose();
+        }
+
+
+        [Test]
+        public void FloatSettles()
+        {
+            m_circuit.Tick();
+
+            Assert.That(!m_bufGate.Output.State.IsError(), "Buffer gate shouldn't error");
+            Assert.That(m_bufGate.Output.State, Is.Not.EqualTo(LineState.Floating), "Buffer gate shouldn't float.");
+        }
+
+        [Test]
+        public void GoesLow()
+        {
+            var pin = new PinComponent(m_circuit);
+
+            m_wire.Pins.Add(pin.Connection);
+
+            pin.Connection.SetState(LineState.Low);
+
+            m_circuit.Tick();
+
+            Assert.That(m_bufGate.Output.State, Is.EqualTo(LineState.Low));
+        }
+
+        [Test]
+        public void GoesHigh()
+        {
+            var pin = new PinComponent(m_circuit);
+
+            m_wire.Pins.Add(pin.Connection);
+
+            pin.Connection.SetState(LineState.High);
+
+            m_circuit.Tick();
+
+            Assert.That(m_bufGate.Output.State, Is.EqualTo(LineState.High));
+        }
+    }
+}
---
+++ Tests/SimTests/NotGateTests.cs
@@ -0,0 +1,71 @@
+﻿using NUnit.Framework;
+
+using SG.Simulator.Components.Gates;
+using SG.Simulator.Components;
+using SG.Simulator;
+using System.Linq;
+
+namespace SimTests
+{
+    internal class NotGateTests
+    {
+        private Circuit m_circuit;
+        private NotGate m_notGate;
+
+        private Wire m_wire;
+
+        [SetUp]
+        public void Setup()
+        {
+            m_circuit = new Circuit();
+            m_notGate = new NotGate(m_circuit);
+            m_wire = new Wire(m_circuit);
+
+            m_wire.Pins.Add(m_notGate.InputPins.Single());
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            m_circuit.Dispose();
+        }
+
+
+        [Test]
+        public void FloatSettles()
+        {
+            m_circuit.Tick();
+
+            Assert.That(!m_notGate.Output.State.IsError(), "NOT gate shouldn't error");
+            Assert.That(m_notGate.Output.State, Is.Not.EqualTo(LineState.Floating), "NOT gate shouldn't float.");
+        }
+
+        [Test]
+        public void GoesLow()
+        {
+            var pin = new PinComponent(m_circuit);
+
+            m_wire.Pins.Add(pin.Connection);
+
+            pin.Connection.SetState(LineState.High);
+
+            m_circuit.Tick();
+
+            Assert.That(m_notGate.Output.State, Is.EqualTo(LineState.Low));
+        }
+
+        [Test]
+        public void GoesHigh()
+        {
+            var pin = new PinComponent(m_circuit);
+
+            m_wire.Pins.Add(pin.Connection);
+
+            pin.Connection.SetState(LineState.Low);
+
+            m_circuit.Tick();
+
+            Assert.That(m_notGate.Output.State, Is.EqualTo(LineState.High));
+        }
+    }
+}
